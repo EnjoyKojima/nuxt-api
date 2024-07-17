3e5e477b65415f7680999ce695f8ee451993f456
EnjoyKojima <majiko2627kojima@gmail.com>
2024-07-17 12:57:35 +0000

    ページを分けた

--- app.vue
+++ app.vue
@@ -1,60 +1,10 @@
 <script setup lang="ts">
-import { useForm } from 'vee-validate'
-import { toTypedSchema } from '@vee-validate/zod'
-import { toast } from '@/components/ui/toast'
-
-const { handleSubmit } = useForm({
-  validationSchema: toTypedSchema(authInputSchema),
-})
-
-const onSubmit = handleSubmit((values) => {
-  toast({
-    title: 'You submitted the following values:',
-    description: h('pre', { class: 'mt-2 w-[340px] rounded-md bg-slate-950 p-4' }, h('code', { class: 'text-white' }, JSON.stringify(values, null, 2))),
-  })
-})
-
-const { $client } = useNuxtApp()
-
-const { data: todos } = await $client.user.getUsers.useQuery()
 </script>
 
 <template>
   <div class="p-10">
-    <NuxtRouteAnnouncer />
     <Toaster />
-    <form class="w-2/3 space-y-6" @submit="onSubmit">
-      <FormField v-slot="{ componentField }" name="username">
-        <FormItem>
-          <FormLabel>Username</FormLabel>
-          <FormControl>
-            <Input type="text" placeholder="good name" v-bind="componentField" />
-          </FormControl>
-          <FormDescription>
-            This is your public display name.
-          </FormDescription>
-          <FormMessage />
-        </FormItem>
-      </FormField>
-      <FormField v-slot="{ componentField }" name="password">
-        <FormItem>
-          <FormLabel>Password</FormLabel>
-          <FormControl>
-            <Input type="password" placeholder="super secure password" v-bind="componentField" />
-          </FormControl>
-          <FormDescription>
-            This is your private password.
-          </FormDescription>
-          <FormMessage />
-        </FormItem>
-      </FormField>
-      <Button type="submit">
-        Submit
-      </Button>
-    </form>
-
-    <div>
-      <p> {{ todos }} </p>
-    </div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
   </div>
 </template>
